660043f659bcbfb58c3409794a4df2297a413db6
Bartłomiej Wąś <63346093+KT-Trez@users.noreply.github.com>
2023-09-22 21:47:13 +0200

    generate ics schedule

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const cuotPageOrigin = 'https://it.pk.edu.pl/';
+export const icsSchedulePatch = 'resources/schedule.ics';
 export const scheduleFilePath = 'resources/schedule.xls';
 export const scheduleUrl = 'https://it.pk.edu.pl/?page=rz';
 export const resourcesDir = 'resources';
