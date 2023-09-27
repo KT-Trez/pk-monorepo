b9aa71926912cf590ac192131c0e6f9fa6a6e39c
Bartłomiej Wąś <63346093+KT-Trez@users.noreply.github.com>
2023-09-28 00:23:53 +0200

    print version at start

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { resourcesDir } from './config';
 import { updateResources } from './services';
 
 if (process.env.DEBUG) {
-  console.info('Starting cron');
+  console.info(`Cron process started: v${process.env.npm_package_version}`);
 }
 
 if (!fs.existsSync(resourcesDir)) {
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DEBUG?: string;
+      npm_package_version: string;
       UPLOAD?: string;
       TORUS_USERNAME?: string;
       TORUS_PASSWORD?: string;
