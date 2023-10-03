465bc52a5691cbad4ce89640e83cf26bcbfad1b9
Bartłomiej Wąś <63346093+KT-Trez@users.noreply.github.com>
2023-10-03 11:21:17 +0200

    update config

--- src/resources/Timetable/xlsParser/xlsParserConfig.ts
+++ src/resources/Timetable/xlsParser/xlsParserConfig.ts
@@ -1,7 +1,7 @@
 import { XlsParserConfig } from './types';
 
 export const secondYearConfig: XlsParserConfig = {
-  classes: [10, 11, 12, 13],
+  classes: [11, 12, 13, 14],
   dateIndex: 0,
   hourIndex: 1,
   hourRegex: /\d?\d:\d\d-\d?\d:\d\d/i,
