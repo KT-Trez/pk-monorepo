161aa253ab49c7973135332641c784391dc49b41
Bartłomiej Wąś <63346093+KT-Trez@users.noreply.github.com>
2025-04-27 17:12:19 +0200

    fix: typecheck

--- apps/server/src/controllers/event.controller.ts
+++ apps/server/src/controllers/event.controller.ts
@@ -1,16 +1,16 @@
-import type { EventApi, EventDb } from '@pk/types/event.js';
-import { BaseController } from '../classes/BaseController.ts';
-import { orm } from '../database/orm.ts';
-import { EventTransformer } from '../transformers/EventTransformer.ts';
-
-class EventController extends BaseController<EventApi, EventDb, 'events'> {
-  constructor() {
-    super({
-      model: 'events',
-      orm,
-      transformer: new EventTransformer(),
-    });
-  }
-}
-
-export const eventController = new EventController();
+// import type { EventApi, EventDb } from '@pk/types/event.js';
+// import { BaseController } from '../classes/BaseController.ts';
+// import { orm } from '../database/orm.ts';
+// import { EventTransformer } from '../transformers/EventTransformer.ts';
+//
+// class EventController extends BaseController<EventApi, EventDb, 'events'> {
+//   constructor() {
+//     super({
+//       model: 'events',
+//       orm,
+//       transformer: new EventTransformer(),
+//     });
+//   }
+// }
+//
+// export const eventController = new EventController();
